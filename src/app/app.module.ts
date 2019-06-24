58ceaa79a6b5f2bc82ab9be3fba22624a795e7f7
tuningfreelance <tuningfreelance@gmail.com>
2019-06-24 12:36:11 -0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
